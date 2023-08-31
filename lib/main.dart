0efd649917efb64ba418aad884f527207ffcbdcf
dhihanaa <dhihanaagustinakolimah@gmail.com>
2023-08-31 09:36:06 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
 class DataFromAPI extends StatelessWidget {
   const DataFromAPI({super.key});
 
@@ -30,7 +29,22 @@ class DataFromAPI extends StatelessWidget {
     List<User> users = [];
 
     for (var u in jsonData) {
-      User user = User(u["name"], u["email"], u["username"]);
+      User user = User(
+        u["name"],
+        u["email"],
+        u["username"],
+        u["address"]["street"],
+        u["address"]["suite"],
+        u["address"]["city"],
+        u["address"]["zipcode"],
+        u["address"]["geo"]["lat"],
+        u["address"]["geo"]["lng"],
+        u["phone"],
+        u["website"],
+        u["company"]["name"],
+        u["company"]["catchPhrase"],
+        u["company"]["bs"],
+      );
       users.add(user);
     }
     print(users.length);
@@ -41,37 +55,80 @@ class DataFromAPI extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('DataAPI'),
+        title: Center(child: Text('Data API')),
       ),
       body: Container(
-        child: Card(
-          child: FutureBuilder(
-              future: getUserData(),
-              builder: (context, snapshot) {
-                if (snapshot.data == null) {
-                  return Container(
-                    child: Center(
-                      child: Text('Loading...'),
-                    ),
-                  );
-                } else
-                  return ListView.builder(
-                      itemCount: snapshot.data.length,
-                      itemBuilder: (context, i) {
-                        return ListTile(
-                          title: Text(snapshot.data[i].name),
-                          subtitle: Text(snapshot.data[i].username),
-                          trailing: Text(snapshot.data[i].email),
-                        );
-                      });
-              }),
-        ),
+        child: FutureBuilder(
+            future: getUserData(),
+            builder: (context, snapshot) {
+              if (snapshot.data == null) {
+                return Container(
+                  child: Center(
+                    child: Text('Loading...'),
+                  ),
+                );
+              } else
+                return ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (context, i) {
+                    return ListTile(
+                      title: Text(snapshot.data[i].name),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(snapshot.data[i].username),
+                          Text(snapshot.data[i].email),
+                          Text(snapshot.data[i].addressstreet),
+                          Text(snapshot.data[i].addresssuite),
+                          Text(snapshot.data[i].addresscity),
+                          Text(snapshot.data[i].addresszipcode),
+                          Text(snapshot.data[i].addressgeolat),
+                          Text(snapshot.data[i].addressgeolng),
+                          Text(snapshot.data[i].phone),
+                          Text(snapshot.data[i].website),
+                          Text(snapshot.data[i].companyname),
+                          Text(snapshot.data[i].companycatchPhrase),
+                          Text(snapshot.data[i].companybs),
+                        ],
+                      ),
+                    );
+                  },
+                );
+            }),
       ),
     );
   }
 }
 
 class User {
-  final String name, email, username;
-  User(this.name, this.email, this.username);
+  final String name,
+      email,
+      username,
+      addressstreet,
+      addresssuite,
+      addresscity,
+      addresszipcode,
+      addressgeolat,
+      addressgeolng,
+      phone,
+      website,
+      companyname,
+      companycatchPhrase,
+      companybs;
+  User(
+    this.name,
+    this.email,
+    this.username,
+    this.addressstreet,
+    this.addresssuite,
+    this.addresscity,
+    this.addresszipcode,
+    this.addressgeolat,
+    this.addressgeolng,
+    this.phone,
+    this.website,
+    this.companyname,
+    this.companycatchPhrase,
+    this.companybs,
+  );
 }
